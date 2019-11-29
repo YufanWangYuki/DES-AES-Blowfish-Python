ccdb35ce6db17b12d7902c43d06302eca3cf47c8
YufanWangYuki <50053568+YufanWangYuki@users.noreply.github.com>
2019-11-29 08:16:27 +0800

    Update generate_file.py

--- generate_file.py
+++ generate_file.py
@@ -1,29 +1,19 @@
-# -*- coding: utf-8 -*-
-# @author: lenovo 
-# @email: wangyufan@shanshu.ai 
-# @date: 2019/11/27
-#!/usr/bin/env python
 #-*- coding:utf-8 -*-
-
 import os
 import random
 
 def genSizeFile(fileName, fileSize):
+    
     #file path
     filePath="./dataset/Data"+fileName+".txt"
 
-    # 生成固定大小的文件
     # date size
     ds = 0
     with open(filePath, "w", encoding="utf8") as f:
         while ds < fileSize:
             char = random.choice('abcdefghijklmnopqrstuvwxyz!@#$%^&*()')
             f.write(str(char))
-            # f.write("\n")
             ds=os.path.getsize(filePath)
-    # print(os.path.getsize(filePath))
-
-# start here.
 
 
 for i in range(20):
